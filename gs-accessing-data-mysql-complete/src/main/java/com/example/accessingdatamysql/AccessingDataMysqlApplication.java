301b3e901a1944896367062df63171fc00154699
ImNotAntMan <ikarusppong@gmail.com>
2022-03-27 17:00:06 +0900

    Initial commit

---
+++ gs-accessing-data-mysql-complete/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -0,0 +1,14 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"com.example.accessingdatamysql"})
+@SpringBootApplication
+public class AccessingDataMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataMysqlApplication.class, args);
+	}
+}
