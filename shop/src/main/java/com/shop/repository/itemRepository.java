fa704b5fb640a8c6e3523865dea1b01ab5958518
ImNotAntMan <ikarusppong@gmail.com>
2022-04-07 19:18:04 +0900

    study

--- shop/src/main/java/com/shop/repository/itemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shop.entity.Item;
-
-public interface itemRepository extends JpaRepository<Item, Long> {
-
-}
